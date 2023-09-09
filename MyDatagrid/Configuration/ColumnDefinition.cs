25ebdbdcb55cc397bff9e683c696f49d12b9bd4c
manhal shiha <manhal6@gmail.com>
2023-09-09 10:30:21 +0300

    sort is finished

--- MyDatagrid/Configuration/ColumnDefinition.cs
+++ MyDatagrid/Configuration/ColumnDefinition.cs
@@ -6,11 +6,13 @@
         {
             this.Alignment = Alignment.NotSet;
             this.DataType= DataType.NotSet;
+            this.SortDirection = SortDirection.NotSet;
         }
         public string? Datafield { get; set; }
         public string? Caption { get; set; }
         public DataType DataType{ get; set; }
         public string? Format { get; set; }
         public Alignment Alignment { get; set; }
+        public SortDirection SortDirection { get; set; }
     }
 }
